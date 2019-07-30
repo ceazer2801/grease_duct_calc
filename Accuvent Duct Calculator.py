65e83a39824652c7fb766656a38aaacab4b0b326
Christian Cathro <48690410+ceazer2801@users.noreply.github.com>
2019-07-30 09:22:54 -0500

    Renamed all var and lists
    added more detailed var names

--- Accuvent Duct Calculator.py
+++ Accuvent Duct Calculator.py
@@ -1,49 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
-numexhfan = int(input("How many EXH fans are there? "))
-if numexhfan <= 1:
-    cfm1 = int(input("What is the CFM of EF1? "))
-    d1_1 = (cfm1/1800*144)
-    d1_2 = d1_1 ** 0.5
-    d1_2 = round(d1_2)
-    m1 = round(cfm1 * .80)
-    m1_1 = (m1/1500*144)
-    m1_2 = round(m1_1 ** 0.5)
-    print("EF1 Set to {} cfm" .format(cfm1))
-    print("MUA1 Set to {} cfm" .format(m1))
-    print("Recommended Exhaust Duct Size for {} cfm = {} square".format(cfm1,d1_2))
-    print("Recommended MUA Duct Size for {} cfm = {} square".format(m1,m1_2))
+num_exh_fan = int(input("How many EXH fans are there? "))
+if num_exh_fan <= 1:
+    exh_cfm = int(input("What is the CFM of EF1? "))
+    exh_duct_size = (exh_cfm/1800*144)
+    exh_duct_size = exh_duct_size ** 0.5
+    exh_duct_size = round(exh_duct_size)
+    mua_cfm = round(exh_cfm * .80)
+    mua_duct_size = (mua_cfm/1500*144)
+    mua_duct_size = round(mua_duct_size ** 0.5)
+    print("EF1 Set to {} cfm" .format(exh_cfm))
+    print("MUA1 Set to {} cfm" .format(mua_duct_size))
+    print("Recommended Exhaust Duct Size for {} cfm = {} square".format(exh_cfm,exh_duct_size))
+    print("Recommended MUA Duct Size for {} cfm = {} square".format(mua_cfm,mua_duct_size))
 else:
-    efanlist = [0]
-    efanlist2 = [0]
-    for cfms in range(0,numexhfan):
-        cfms = int(input("Please Enter the CFM's of your EXH fans :"))
-        efanlist.append(cfms)
-        duct = (cfms/1800*144)
-        duct = duct ** 0.5
-        duct = round(duct)
-        efanlist2.append(duct)
-    for ecfms in efanlist2:
-        if ecfms > 0:
-            print("Exhaust Duct Size is:",ecfms,)
-    nummuafan = int(input("How many MUA fans are there? "))
-    mfanlist = [0]
-    mfanlist2 = [0]
-    if nummuafan >= 1:
-        for cfms2 in range(0,nummuafan):
-            cfms2 = int(input("Please Enter the CFM's of your MUA fans: "))
-            mfanlist.append(cfms2)
-            mduct = (cfms2/1500*144)
+    exh_cfm_list = [0]
+    exh_duct_list = [0]
+    for exh_cfms in range(0,num_exh_fan):
+        exh_cfms = int(input("Please Enter the CFM's of your EXH fans :"))
+        exh_cfm_list.append(cfms)
+        exh_duct_size = (exh_cfms/1800*144)
+        exh_duct_size = exh_duct_size ** 0.5
+        exh_duct_size = round(exh_duct_size)
+        exh_duct_list.append(exh_duct_size)
+    for final_exh_duct_size in exh_duct_list:
+        if final_exh_duct_size > 0:
+            print("Exhaust Duct Size for", " is:",final_exh_duct_size,)
+    num_mua_fan = int(input("How many MUA fans are there? "))
+    mua_cfm_list = [0]
+    mua_duct_list = [0]
+    if num_mua_fan >= 1:
+        for mua_cfms in range(0,num_mua_fan):
+            mua_cfms = int(input("Please Enter the CFM's of your MUA fans: "))
+            mua_cfm_list.append(mua_cfms)
+            mduct = (mua_cfms/1500*144)
             mduct = mduct ** 0.5
             mduct = round(mduct)
-            mfanlist2.append(mduct)
-        for mcfms2 in mfanlist2:
-            if mcfms2 > 0:
-                print("Make-Up Air Duct Size is:",mcfms2,)
+            mua_duct_list.append(mduct)
+        for final_mua_duct_size in mua_duct_list:
+            if final_mua_duct_size > 0:
+                print("Make-Up Air Duct Size is:",final_mua_duct_size,)
 
             
     
